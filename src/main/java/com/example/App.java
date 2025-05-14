b1812ad87f75768b754083e2df7cd63c725bdf1a
varshitha5111 <varshithabkalmath@gmail.com>
2025-05-14 20:24:37 +0530

    first

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ImmutableList<String> list = ImmutableList.of("hello","world","hii");
+        System.out.println( list );
+        File source = new File("source.txt");
+        File dest = new File("dest.txt");
+        try{
+        	FileUtils.copyFile(source,dest);
+  	        System.out.println("success");
+        	
+        }
+        catch(IOException e){
+        	System.out.println("error"+e);
+        }
+    }
+}
